806956f38d13166279f39ea668f078d1a1223478
veroyols <vero.scholz@gmail.com>
2025-10-10 15:47:24 -0300

    public

--- src/services/PublicServices.ts
+++ src/services/PublicServices.ts
@@ -1,19 +1,5 @@
-import type { ResponsePublicRange } from "../types/ResponsePublicRange";
 import type { ResponsePublicSummary } from "../types/ResponsePublicSummary";
 import { DOTNET_PUBLIC_URL } from "../config/constant";
-
-//https://localhost:32773/api/Public/ensembl?chrom=${chr}&start=${start}&end=${end}`;
-const EnsemblService = async (
-    chr: string,
-    start: number,
-    end: number
-): Promise<ResponsePublicRange> => {
-    const url = `${DOTNET_PUBLIC_URL}/ensembl?chrom=chr${chr}&start=${start}&end=${end}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    return data;
-};
 
 //https://localhost:32769/api/Public/summary?entrez=${entrez}&type=${gene}
 const SummaryService = async (
@@ -26,4 +12,4 @@ const SummaryService = async (
     return data;
 };
 
-export { EnsemblService, SummaryService };
+export { SummaryService };
