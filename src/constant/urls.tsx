ce8e55cbeecc975f1e9ae76a619478e7e4e3b7d1
veroyols <vero.scholz@gmail.com>
2025-12-17 03:47:54 -0300

    domain

--- src/constant/urls.tsx
+++ src/constant/urls.tsx
@@ -1,7 +1,7 @@
-const AZURE: string = "http://20.82.6.107/80";
+const DOMAIN: string = "httpS://vitamina-unaj.duckdns.org";
 
-export const DOTNET_BIOCONDUCTOR_URL: string = `${AZURE}/api/Plumber`;
-export const DOTNET_PUBLIC_URL: string = `${AZURE}/api/Public`;
-export const DOTNET_BLAST_URL: string = `${AZURE}/api/Blast`;
-export const DOTNET_FOLD_URL: string = `${AZURE}/api/Folding`;
-export const DOTNET_PYTHON_URL: string = `${AZURE}/api/Python`;
+export const DOTNET_BIOCONDUCTOR_URL: string = `${DOMAIN}/api/Plumber`;
+export const DOTNET_PUBLIC_URL: string = `${DOMAIN}/api/Public`;
+export const DOTNET_BLAST_URL: string = `${DOMAIN}/api/Blast`;
+export const DOTNET_FOLD_URL: string = `${DOMAIN}/api/Folding`;
+export const DOTNET_PYTHON_URL: string = `${DOMAIN}/api/Python`;
